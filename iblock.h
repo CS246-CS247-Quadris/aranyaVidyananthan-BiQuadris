6805567068889ad9316c5944fcff543df0f028a8
Saranya Vidyananthan <43966650+SaranyaVidyananthan@users.noreply.github.com>
2018-11-25 14:01:39 -0500

    Update iblock.h

--- iblock.h
+++ iblock.h
@@ -1,18 +1,24 @@
 #ifndef _IBLOCK_H
 #define _IBLOCK_H
+#include "block.h"
 
 using namespace std;
 
 class IBlock : public Block{
 
-   public:
-   IBlock();
-   ~IBlock();
-   bool move(int direction)override;
-   const char getType()override;
-   bool rotate(int direction)override;
-   void attachCell(Cell* c)override;
-   void detachCell(Cell* c)override;
+public:
+    IBlock( Board * g );
+    ~IBlock();
+
+    // sets the block in its first pos, true if successful
+    bool set() override;
+    // moves the block one point to the left, right or down, return true if successful
+    bool move( int direction ) override;
+    // returns the type of the block
+    const char getType() override;
+    // rotates the block one turn clockwise or counterclockwise, return true if successful
+    bool rotate( int direction ) override;
+
 };
 
 #endif
