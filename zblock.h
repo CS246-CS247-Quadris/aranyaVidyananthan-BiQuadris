940f156b4c83ad76bc57c82133a651b3e3501c79
Saranya Vidyananthan <43966650+SaranyaVidyananthan@users.noreply.github.com>
2018-11-25 14:16:56 -0500

    Update zblock.h

--- zblock.h
+++ zblock.h
@@ -1,18 +1,24 @@
 #ifndef _ZBLOCK_H
 #define _ZBLOCK_H
+#include "block.h"
 
 using namespace std;
 
 class ZBlock : public Block{
 
-   public:
-   ZBlock();
-   ~ZBlock();
-   bool move(int direction)override;
-   const char getType()override;
-   bool rotate(int direction)override;
-   void attachCell(Cell* c)override;
-   void detachCell(Cell* c)override;
+public:
+    ZBlock( Board * g );
+    ~ZBlock();
+
+    // sets the block in its first pos, true if successful
+    bool set() override;
+    // moves the block one point to the left, right or down, return true if successful
+    bool move( int direction ) override;
+    // returns the type of the block
+    const char getType() override;
+    // rotates the block one turn clockwise or counterclockwise, return true if successful
+    bool rotate( int direction ) override;
+
 };
 
 #endif
