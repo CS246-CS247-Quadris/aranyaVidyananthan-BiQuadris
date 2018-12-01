d2ad430c885e6ce9bc6dcfd7262e269828910606
s2vidyan <s2vidyan@student.cs.uwaterloo.ca>
2018-12-01 13:47:30 -0500

    fixed board print

--- board.h
+++ board.h
@@ -17,25 +17,36 @@ protected:
     int levelnum;
     Block* nextBlock;
     int score;
-   
+  
+    int clearLines(int i); 
 public:
     Board(int player);
     ~Board();
-
+    // returns the current block on the board
     Block * getNextBlock();
+    // returns the next block on the board
     Block * getCurrBlock();
-    bool setNewBlock();
-    Cell* getCell(int i, int j);
-    void createBlock(string t);
-    int clearLines(int i);
-    void setLevel(int l);
-    void clearBoard();
-    int getLevel();
-    int getScore();
-    int getPlayer();
-    void detach(Block* b);
-    void print();
+    // sets the first block of the board using the level
     void setFirstBlock(string t);
+    // creates the next block based on the level of the player
+    void createBlock(string t);
+    // sets the next block to the current block on the board
+    bool setNewBlock();
+    // returns the cell at the given (i, j)
+    Cell* getCell(int i, int j);
+    // sets the level to the given level
+    void setLevel(int l);
+    // checks if any of the rows are full and removes them
+    void clearBoard();
+    // returs the level of the player
+    int getLevel();
+    // returns the score of the player
+    int getScore();
+    // returns the player number
+    int getPlayer();
+    // prints the board
+    void print();
+
 };
 
 #endif
