f0b4b586d12fc8d89c13a94d5eaf62dc16ef59c0
s2vidyan <s2vidyan@student.cs.uwaterloo.ca>
2018-12-02 14:15:00 -0500

    Adding graphics

--- block.cc
+++ block.cc
@@ -6,14 +6,17 @@
 Block::Block(int levelcreated, char type, Board * g ):shape{}, 
     temp{}, levelcreated{levelcreated},type{type}, orientation{1}, g{g}, removed{0}, scale{17}{}
 
+
 Block::~Block() {}
+
 
 void Block::clearLine(int rowNum){
     int index = shape.size();
     if(shape.empty()){return;}
     char c = shape[0]->getType();
     for(int i = 0; i < index; i ++){
-        if(shape[i]->getY() >rowNum){
+        g->getDisplay()->fillRectangle( (shape[index]->getX() + g->getX())*scale, (shape[index]->getY() + g->getY())*scale, scale, scale, 0 );
+        if(shape[i]->getY() > rowNum){
             temp.emplace_back(g->getCell(shape[i]->getX(),shape[i]->getY()));
             //in this case, it is just the same cell originally in shape
         }else if(shape[i]->getY() == rowNum){
@@ -34,10 +37,31 @@ void Block::clearLine(int rowNum){
         shape.emplace_back(temp[i]);
         shape.back()->setType(c);
         shape.back()->setStatus(false);
+        // draws the block based on the type of the block
+        if ( c == 'I' ) {
+            g->getDisplay()->fillRectangle( (shape.back()->getX() + g->getX())*scale, (shape.back()->getY() + g->getY())*scale, scale, scale, 2 );
+        }
+        else if ( c == 'J' ) {
+            g->getDisplay()->fillRectangle( (shape.back()->getX() + g->getX())*scale, (shape.back()->getY() + g->getY())*scale, scale, scale, 3 );
+        }
+        else if ( c == 'L' ) {
+            g->getDisplay()->fillRectangle( (shape.back()->getX() + g->getX())*scale, (shape.back()->getY() + g->getY())*scale, scale, scale, 4 );
+        }
+        else if ( c == 'S' ) {
+            g->getDisplay()->fillRectangle( (shape.back()->getX() + g->getX())*scale, (shape.back()->getY() + g->getY())*scale, scale, scale, 5 );
+        }
+        else if ( c == 'T' ) {
+            g->getDisplay()->fillRectangle( (shape.back()->getX() + g->getX())*scale, (shape.back()->getY() + g->getY())*scale, scale, scale, 6 );
+        }
+        else if ( c == 'O' ) {
+            g->getDisplay()->fillRectangle( (shape.back()->getX() + g->getX())*scale, (shape.back()->getY() + g->getY())*scale, scale, scale, 7 );
+        }
+        else {
+            g->getDisplay()->fillRectangle( (shape.back()->getX() + g->getX())*scale, (shape.back()->getY() + g->getY())*scale, scale, scale, 8 );
+        }
     }
     temp.clear();
     if(shape.size()==0){g->addScorefromBlock(levelcreated);}
- 
-
-
 }
+
+
